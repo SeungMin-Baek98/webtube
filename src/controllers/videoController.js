15dfba96b5b3efec2a3358ec4db8cf436feb83fc
SeungMin Baek <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-09-06 15:54:41 +0900

    pug template에서도 조건 상태를 이용하여 상태변화를 할 수 있다

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,12 @@
-export const trending = (req, res) => res.render("home");
+const fakeUser = {
+  username: "승민",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.send("Edit");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("UPload");
+export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("DeleteVideo");
