cd4b84b5959ed38a9026f7e0a449aed6836492c0
SeungMin Baek <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-09-14 17:09:38 +0900

    로그인 validation 로직 커밋

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,72 @@
-import videoModel from "../models/videoModel";
+import userModel from "../models/userModel";
+import bcyrpt from "bcrypt";
 
-export const join = (req, res) => {
+export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Join" });
 };
+
+export const postJoin = async (req, res) => {
+  const { email, username, password, rePassword, name, location } = req.body;
+  const pageTitle = "Join";
+
+  if (password !== rePassword) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "비밀번호가 일치하지 않습니다.",
+    });
+  }
+
+  const exists = await userModel.exists({ $or: [{ username }, { email }] });
+
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "이메일 혹은 이름이 사용중입니다.",
+    });
+  }
+
+  try {
+    // userDB를 mongoDB 테이블에 넣는 로직.
+    await userModel.create({
+      email,
+      username,
+      password,
+      name,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res
+      .status(400)
+      .render("join", { pageTitle, errorMessage: error._message });
+  }
+};
+
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: "Login" });
+};
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const loginUser = await userModel.findOne({ username });
+
+  if (!loginUser) {
+    return res
+      .status(400)
+      .render("login", { pageTitle, errorMessage: "계정이 없습니다." });
+  }
+  const loginPass = await bcyrpt.compare(password, loginUser.password);
+  if (!loginPass) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "패스워드가 일치하지않습니다.",
+    });
+  }
+  res.end();
+};
+
 export const edit = (req, res) => res.send("Edit");
 export const remove = (req, res) => res.send("DeleteUser");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("see");
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,13 +1,17 @@
-import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  // rePassword: { type: String, required: true },
   name: { type: String, required: true },
-  // nickname: { type: String, required: true },
   location: String,
+});
+
+//User가 db에 저장되기 이전에 그정보를 가료채와서 password를 hash된 정보로 저장하는 로직.
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const userModel = mongoose.model("User", userSchema);
