c84441579584e794f3444356299214920a304a9d
SeungMin Baek <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-09-23 18:25:51 +0900

    commit3

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,9 +1,7 @@
 // pug template 과 user data 공유
 export const localsMiddelware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.loggedInUser = req.session.user
-    ? req.session.user
-    : req.session.name;
+  res.locals.loggedInUser = req.session.user;
   res.locals.siteName = "Wetube";
   next();
 };
